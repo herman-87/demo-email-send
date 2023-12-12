21c6b1814333ae327877573f0659e9b5b5c18001
herman-87 <hermanessoungou@gmail.com>
2023-12-12 21:17:53 +0100

    add java mail dependencie and inject it in the service

---
+++ src/main/java/com/example/demo/service/EmailService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+}
